527eddf2a2862d971c7e79e52edb7efcfd453275
xrole <xrolediamond@gmail.com>
2025-05-25 02:05:27 +0100

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('group name', () {});
+  group('group name', () {
+    test("Test sonar", () {
+      final safetyKey = "test safety key";
+
+      expect(safetyKey, "test safety key");
+    });
+  });
 }
