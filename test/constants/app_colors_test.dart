205d85942da3cb5d84f896fac9262ae0107cf1de
xrole <xrolediamond@gmail.com>
2025-05-25 02:42:59 +0100

    add test for app color class

---
+++ test/constants/app_colors_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pro_mobile/constants/app_colors.dart';
+// Replace with actual path
+
+void main() {
+  group('AppColors', () {
+    test('primary color should be correct', () {
+      expect(AppColors.primary, const Color(0xff1F507B));
+    });
+
+    test('primary2 color should be same as primary', () {
+      expect(AppColors.primary2, AppColors.primary);
+    });
+  });
+}
